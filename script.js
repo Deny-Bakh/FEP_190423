6fbd858837a76ec0edeaceb5c0fe76f21da345e1
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-04-25 10:37:16 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-numberOne = prompt ('Write a random number.');
-numberTwo = prompt ('Write another random number and watch th magic happen.');
-
-numberOne = parseFloat(numberOne);
-numberTwo = parseFloat(numberTwo);
-
-plusNumbers = numberOne + numberTwo;
-extractNumbers = numberOne - numberTwo;
-multiplyNumbers = numberOne * numberTwo;
-divideNumbers = numberOne / numberTwo;
-
-alert(`${numberOne}+${numberTwo}=${plusNumbers}\n${numberOne}-${numberTwo}=${extractNumbers}\n${numberOne}*${numberTwo}=${multiplyNumbers}\n${numberOne}/${numberTwo}=${divideNumbers}`);
+hoursNumber = prompt ('Write down random number of hours, please.');
+secondsNumber = hoursNumber * 60 * 60;
+alert (`In ${hoursNumber} hours there are ${secondsNumber} seconds.`);
