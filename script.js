e9dbe6894e97121a6df60a600c9337d4fcdd74bd
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-04-29 08:28:42 +0300

    math

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-yourName = prompt ('What is your name traveller?');
-alert ('Hello,' + ' ' + yourName + '! How are you?');
+variantNumberOne = prompt ('Please write a random number and watch a magic happen');
+variantNumberTwo = prompt ('Please write another random number and this time there will be magic for sure');
+variantNumberThree = prompt ('We know that we ask a lot but can you please write another number and you will see magic 100%');
+
+variantNumberOne = parseFloat(variantNumberOne);
+variantNumberTwo = parseFloat(variantNumberTwo);
+variantNumberThree = parseFloat(variantNumberThree);
+
+averageOfNumbers = (variantNumberOne + variantNumberTwo + variantNumberThree)/3;
+
+alert( 'This is your average number' + ' ' + averageOfNumbers + ', ' + 'which appeared from your numbers.\nMagic');
