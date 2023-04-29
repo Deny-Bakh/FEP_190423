136d2653b5c4e76e1c78f891b8fdedd3a474dc01
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-04-29 09:36:40 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,11 +1,90 @@
-variantNumberOne = prompt ('Please write a random number and watch a magic happen');
-variantNumberTwo = prompt ('Please write another random number and this time there will be magic for sure');
-variantNumberThree = prompt ('We know that we ask a lot but can you please write another number and you will see magic 100%');
+// birthYear = prompt('What is your bearth year?');
+// placeOfHabitat = prompt ('What city do you live in?');
+// sportLove = prompt ('What is your favourite sport?')
+// currentYear = new Date().getFullYear();
+// age = currentYear - birthYear;
 
-variantNumberOne = parseFloat(variantNumberOne);
-variantNumberTwo = parseFloat(variantNumberTwo);
-variantNumberThree = parseFloat(variantNumberThree);
+// if (placeOfHabitat ===  'Kyiv' || placeOfHabitat ===  'Washington' || placeOfHabitat ===  'London'){
+//     capitalLocated = ''
+//     switch (placeOfHabitat) {
+//         case 'Kyiv':
+//             capitalLocated = 'Ukraine';
+//             break;
+//         case 'Washington':
+//             capitalLocated = 'USA';
+//             break;
+//         case 'London':
+//             capitalLocated = 'Great Britain';
+//                 break;
+//     }
+//     alert('You are' + ' ' + age + ' years old! And you live in a capital of' + ' ' + capitalLocated + ', congrats!')
+// }
+// else {
+//     alert ('You are' + ' ' + age + ' years old! And you live in' + ' ' + placeOfHabitat)
+// };
 
-averageOfNumbers = (variantNumberOne + variantNumberTwo + variantNumberThree)/3;
+// ***************************************************************************************************************************************
 
-alert( 'This is your average number' + ' ' + averageOfNumbers + ', ' + 'which appeared from your numbers.\nMagic');
+
+birthYear = prompt('What is your birth year?');
+if(birthYear !== null){
+    placeOfHabitat = prompt ('What city do you live in?');
+    if(placeOfHabitat !== null){
+        favouriteSport = prompt ('What is your favourite sport?');
+        if(favouriteSport !== null){
+            currentYear = new Date().getFullYear();
+            age = currentYear - birthYear;
+            message = 'You are' + ' ' + age + ' years old!' + ' ' + '\n';
+            if (placeOfHabitat ===  'Kyiv' || placeOfHabitat ===  'Washington' || placeOfHabitat ===  'London'){
+                capitalLocated = ''
+                switch (placeOfHabitat) {
+                    case 'Kyiv':
+                        capitalLocated = 'Ukraine';
+                        break;
+                    case 'Washington':
+                        capitalLocated = 'USA';
+                        break;
+                    case 'London':
+                        capitalLocated = 'Great Britain';
+                            break;
+                }
+                message += 'And you live in a capital of' + ' ' + capitalLocated + ', congrats!' + '\n';
+            }
+            else {
+                message += 'And you live in' + ' ' + placeOfHabitat + '\n';
+            };
+
+            // if (favouriteSport.toLowerCase() === 'football') {
+            //     message += 'Cool! You want to be like Lionel Messi!';
+            //   } else if (favouriteSport.toLowerCase() === 'boxing') {
+            //     message += 'Cool! You want to be like Vitalii Klychko!';
+            //   } else if (favouriteSport.toLowerCase() === 'hockey') {
+            //     message += 'Cool! You want to be like Wayne Gretzky';
+            //   };
+
+            if (favouriteSport.toLowerCase() ===  'hockey' || favouriteSport.toLowerCase() ===  'boxing' || favouriteSport.toLowerCase() ===  'football'){
+                sportPerson = ''
+                switch (favouriteSport.toLowerCase()) {
+                    case 'hockey':
+                        sportPerson = 'Wayne Gretzky';
+                        break;
+                    case 'boxing':
+                        sportPerson = 'Vitalii Klychko';
+                        break;
+                    case 'football':
+                        sportPerson = 'Lionel Messi';
+                            break;
+                }
+                message += 'Cool! You want to be like' + ' ' + sportPerson + '!';
+            }
+            else {
+                message += 'You have chosen a great way to spend your physical energy! Keep on working hard!';
+            };
+            alert(message);
+        } 
+        else{ alert('Too bad you don\'t want to share your favourite sport!')};
+    }
+    else{alert('Too bad you don\'t want to share the city you live in!')};
+}    
+else{alert('Too bad you don\'t want to share your age!');
+};
