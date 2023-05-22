1f88ab0006cbe66936e1911fc0f343d3c46f6beb
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-05-23 00:33:48 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,72 +1,91 @@
-hero = ['Ivan'];
-native = ['York', 'Of'];
-destination = ['Poltava', 'In'];
-
-
-rainbow = destination.concat(native, hero);
-
-rainbow = rainbow.reverse();
-
-rainbow.splice(0, 1, 'Richard');
-// rainbow.shift();
-// rainbow.unshift('Richard');
-// rainbow.splice(3, 0, 'Gave', 'Battle');
-// console.log(rainbow)
-rainbow.splice(3, 2, 'Gave', 'Battle', 'In', 'Vain');
-// rainbow.pop();
-// rainbow.push('Vain');
-// rainbow.splice(6, 1, 'Vain');
-
-colors = ['red', 'orange', 'yellow', 'green', 'lightblue', 'blue', 'purple'];
-  
-document.write('<div class="container">');
-  for (i = 0; i < rainbow.length; i++) {
-    // color = colors[i];
-    // word = rainbow[i];
-    
-    document.write('<div class="wrapper">');
-    document.write(`<div class="colors" style="background-color: ${colors[i]};"></div>`);
-    document.write(`<div class="rainbow"> ${rainbow[i]} </div>`);
-    document.write('</div>');
+function removeElement(array, item) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+      i--;
+    }
   }
-document.write('</div>');
+  return array;
+};
 
-//   document.write('<div class="container">');
-//   for (i = 0; i < rainbow.length; i++) {
-//     document.write('<div class="wrapper">');
-  
-//   color = '';
-//   switch (i) {
-//     case 0:
-//       color = 'red';
-//       break;
-//     case 1:
-//       color = 'orange';
-//       break;
-//     case 2:
-//       color = 'yellow';
-//       break;
-//     case 3:
-//       color = 'green';
-//       break;
-//     case 4:
-//       color = 'lightblue';
-//       break;
-//     case 5:
-//       color = 'blue';
-//       break;
-//     case 6:
-//       color = 'purple';
-//       break;
-//     default:
-//       color = 'black';
+array = [1, 2, 3, 4, 5, 6, 7, 5, 8, 5, 9, 5, 10, 5];
+removeElement(array, 5);
+console.log(array);
+removeElement(array, 3);
+console.log(array);
+removeElement(array, 1);
+console.log(array);
+// ********************************************************************************
+
+// function removeElement(array, item) {
+//   i = 0;
+//   while (i < array.length) {
+//     if (array[i] === item) {
+//       array.splice(i, 1);
+//     } else {
+//       i++;
+//     }
 //   }
-  
-//   document.write(`<div class="colors" style="background-color: ${color};"></div>`);
-//   document.write(`<div class="rainbow">${rainbow[i]}</div>`);
-//   document.write('</div>');
+//   return array;
 // }
-// document.write('</div>');
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 5, 8, 5, 9, 5, 10, 5];
+// removeElement(array, 5);
+// console.log(array);
+
+// ********************************************************************************
+
+// Це найкраще дитятко, бо воно не кромсає перший масив і дає можливість, юзати його знову і знову. Але наскільки я розумію, це не було умовою.
+
+// function removeElement (array, item) {
+// 	resultArray = [];
+  
+//   for (i = 0; i < array.length; i++) {
+//   	if (array[i] !== item) {
+//     	resultArray.push(array[i])
+//     }
+//   }
+//   console.log(resultArray)
+//   return resultArray;
+// }
+
+// array = [1, 2, 3, 4, 5, 6, 7, 5, 8, 5, 9];
+// removeElement(array, 5);
+// removeElement(array, 1);
+// removeElement(array, 2);
+
+// або виводити таким чином:
+// resultArray = removeElement(array, 5);
+// console.log(resultArray);
+
+// ********************************************************************************
+
+// Видаляє тільки перший item, інші не зачіпає. Не повноцінний метод.
+
+// function removeElement(array, item) {
+//   itemToRemove = array.indexOf(item)
+//   if (itemToRemove !== -1) {
+//     array.splice(itemToRemove, 1);
+//   }
+//   return array;
+// };
+
+// array = [1, 2, 3, 4, 5, 6, 7, 5, 8, 9];
+// removeElement(array, 5 );
+// console.log(array);
+
+// function removeElement(array, item) {
+//   itemToRemove = array.indexOf(item)
+//   while (itemToRemove !== -1) {
+//     array.splice(itemToRemove, 1);
+//     itemToRemove = array.indexOf(item);
+//   }
+//   return array;
+// };
+
+// array = [1, 2, 3, 4, 5, 6, 7, 5, 8, 5, 9];
+// removeElement(array, 5 );
+// console.log(array);
 
 
 
