af8a87bd9d79a50768c83d59a2447b6df4b33a50
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-05-22 10:34:16 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,135 +1,73 @@
-numberArray = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-positiveSum = 0;
-countPositive = 0;
+hero = ['Ivan'];
+native = ['York', 'Of'];
+destination = ['Poltava', 'In'];
 
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] > 0) {
-        positiveSum += numberArray[i];
-        countPositive++;
-    }
-}
 
-console.log(`Sum of positive elements: ${positiveSum}`);
-console.log(`Number of positive elements in your array: ${countPositive}`);
+rainbow = destination.concat(native, hero);
 
-// *******************************************************************************************************************
+rainbow = rainbow.reverse();
 
-smallestNumber = numberArray[0];
+rainbow.splice(0, 1, 'Richard');
+// rainbow.shift();
+// rainbow.unshift('Richard');
+// rainbow.splice(3, 0, 'Gave', 'Battle');
+// console.log(rainbow)
+rainbow.splice(3, 2, 'Gave', 'Battle', 'In', 'Vain');
+// rainbow.pop();
+// rainbow.push('Vain');
+// rainbow.splice(6, 1, 'Vain');
 
-for (i = 0; i < numberArray.length; i++){
-    if (numberArray[i] < smallestNumber) {
-        smallestNumber = numberArray[i];
-        indexOfSmallest = numberArray.indexOf(smallestNumber);
-    }
-}
+  colors = ['red', 'orange', 'yellow', 'green', 'lightblue', 'blue', 'purple'];
+  
+  document.write('<div class=main_box>');
+  for (i = 0; i < rainbow.length; i++) {
+    // color = colors[i];
+    // word = rainbow[i];
+    
+    document.write('<div class="wrapper">');
+    document.write(`<div class="colors" style="background-color: ${colors[i]};"></div>`);
+    document.write(`<div class="rainbow"> ${rainbow[i]} </div>`);
+    document.write('</div>');
+  }
+  document.write('</div>');
 
-console.log(`The smallest number in your array is: ${smallestNumber}`);
-console.log(`The index of smallest number in your array is: ${indexOfSmallest}`);
+//   document.write('<div class=main_box>');
+//   for (i = 0; i < rainbow.length; i++) {
+//     document.write('<div class="wrapper">');
+  
+//   color = '';
+//   switch (i) {
+//     case 0:
+//       color = 'red';
+//       break;
+//     case 1:
+//       color = 'orange';
+//       break;
+//     case 2:
+//       color = 'yellow';
+//       break;
+//     case 3:
+//       color = 'green';
+//       break;
+//     case 4:
+//       color = 'lightblue';
+//       break;
+//     case 5:
+//       color = 'blue';
+//       break;
+//     case 6:
+//       color = 'purple';
+//       break;
+//     // default:
+//     //   color = 'black';
+//   }
+  
+//   document.write(`<div class="colors" style="background-color: ${color};"></div>`);
+//   document.write(`<div class="rainbow">${rainbow[i]}</div>`);
+//   document.write('</div>');
+// }
+// document.write('</div>');
 
-// *******************************************************************************************************************
-
-largestNumber = numberArray[0];
-
-for (i = 0; i < numberArray.length; i++){
-    if (numberArray[i] > largestNumber) {
-        largestNumber = numberArray[i];
-        indexOfLargest = numberArray.indexOf(largestNumber);
-    }
-}
-
-console.log(`The largest number in your array is: ${largestNumber}`);
-console.log(`The index of largest number in your array is: ${indexOfLargest}`);
-
-// *******************************************************************************************************************
-
-countNegative = 0;
-
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] < 0) {
-        countNegative++;
-    }
-}
-
-console.log(`Number of negative elements in your array: ${countNegative}`);
-
-// *******************************************************************************************************************
-
-countOddPositive = 0;
-
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] > 0 && numberArray[i] % 2 !== 0) {
-        countOddPositive++;
-    }
-}
-
-console.log(`Number of odd positive elements in your array: ${countOddPositive}`);
-
-// *******************************************************************************************************************
-
-countEvenPositive = 0;
-
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] > 0 && numberArray[i] % 2 === 0) {
-        countEvenPositive++;
-    }
-}
-
-console.log(`Number of even positive elements in your array: ${countEvenPositive}`);
-
-evenPositiveSum = 0;
-
-// *******************************************************************************************************************
-
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] > 0 && numberArray[i] % 2 === 0) {
-        evenPositiveSum += numberArray[i];
-    }
-}
-
-console.log(`Sum of even positive elements in your array: ${evenPositiveSum}`);
-
-// *******************************************************************************************************************
-
-oddPositiveSum = 0;
-
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] > 0 && numberArray[i] % 2 !== 0) {
-        oddPositiveSum += numberArray[i];
-    }
-}
-
-console.log(`Sum of odd positive elements in your array: ${oddPositiveSum}`);
-
-// *******************************************************************************************************************
-
-positiveProduct = 1;
-
-for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] > 0) {
-        positiveProduct *= numberArray[i];
-    }
-}
-
-console.log(`Product of positive elements: ${positiveProduct}`);
-
-// *******************************************************************************************************************
-
-largestNumber = numberArray[0];
-
-for (i = 0; i < numberArray.length; i++){
-    if (numberArray[i] > largestNumber) {
-        largestNumber = numberArray[i];
-    }
-} 
-
-for (i = 0; i < numberArray.length; i++){
-    if (numberArray[i] != largestNumber) {
-        numberArray[i] = 0;
-    }
-} 
-
-console.log(`The largest number in your array is: ${largestNumber}`);
-console.log(`All the other numbers except the largest have become: ${numberArray}`);
 
 
 
