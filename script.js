53d72fa7f9a9ea7670becdbb580fb69036ca83b8
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-05-27 10:48:02 +0300

    work in progress

--- script.js
+++ script.js
@@ -13,3 +13,18 @@
     console.log(sum(3));
     console.log(sum(5));
     console.log(sum(7));
+
+    // function createSum() {
+    //   let total = 0;
+
+    //   return function innerSum(number) {
+    //     total += number;
+    //     return total;
+    //   };
+    // }
+
+    // const sum = createSum()
+
+    // console.log(sum(3));
+    // console.log(sum(5));
+    // console.log(sum(7));
