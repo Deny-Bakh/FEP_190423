c50c60449fc73f2ca27c715e0f94c242f03a3596
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-05-31 10:26:27 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,82 +1,23 @@
-// *****************************************************************************************************************
+// let num;
+// let degree;
 
-cardRow = [];
-
-cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
-
-cardSuits = [
-  ['clubs', '#353c4d'],
-  ['spades', '#353c4d'],
-  ['diamonds', 'red'],
-  ['hearts', 'red'],
-];
-
-// cardSuits = [
-//   ['clubs', 'black'],
-//   ['spades', 'black'],
-//   ['diamonds', 'red'],
-//   ['hearts', 'red'],
-// ];
-
-cardsWithImages = [
-  ['J', 'jack'],
-  ['Q', 'queen'],
-  ['K', 'king'],
-  ['T', 'ace'],
-];
-
-const fullDeckWithoutSuits = cardValues.concat(cardsWithImages);
-
-  function resultDeck() {
-    const fullDeck = [];
-    for (let i = 0; i <fullDeckWithoutSuits.length; i++) {
-      for (let j = 0; j < cardSuits.length; j++) {
-      fullDeck.push([fullDeckWithoutSuits[i], cardSuits[j]]);
-    }
-  }
-  return fullDeck;
-};
-
-const deck = resultDeck();
-console.log(deck);
-
-function createCellInfo(data) {
-  return `<div class="suit_number">
-    <span style="color:${data[1][1]}">${Array.isArray(data[0]) ? data[0][0] : data[0]}</span>
-    <img src="./images/${data[1][0]}.svg" alt="${data[1][0]}">
-</div>`;
+function pow(num,degree) {
+  return degree === 0 ? 1 : degree === 1 ? num : degree < 0 ? 1 / pow(num, -degree) : num * pow(num, degree -1);
 }
 
-function createCell(data) {
-  let cell = '';
-  if (data) {
+// function pow(num, degree) {
+//   if (degree === 0) {
+//     return 1;
+//   } else if (degree === 1) {
+//     return num;
+//   } else if (degree < 0) {
+//     return 1 / pow (num, -degree);
+//   } else {
+//     return num * pow(num, degree - 1);
+//   }
+// }
 
-    cell = `<td class="card_board_td">
-      ${createCellInfo(data)}
-    <div class="main">
-    ${Array.isArray(data[0]) ? `<img src="./images/${data[0][0] === 'T' ? data[1][0] : data[0][1]}.svg" alt="${data[0][1]}">` : ''}
-    </div>
-      ${createCellInfo(data)}
-    </td>`
-  }
-  return cell
-}
-
-  let count = 0;
-  for (tableRow = 0; tableRow < 7; tableRow++) {
-    cardCell = [];
-    for (tableCell = 0; tableCell < 8; tableCell++) {
-      let data = '';
-      if (tableRow === 6 && tableCell === 0) {
-        data = `<td></td><td></td>`
-      }
-      data += createCell(deck[count]);
-      count++;
-      cardCell.push(data);
-    }
-    cardRow.push(`<tr>${cardCell.join('')}</tr>`);
-  }
-
-  document.write(
-  `<table class="card_board">${cardRow.join('')}</table>`
-);
+console.log(pow(2, 0));
+console.log(pow(3, 1));
+console.log(pow(4, -2));
+console.log(pow(5, 3));
