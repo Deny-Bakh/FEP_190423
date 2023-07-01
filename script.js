d13f4940d3f1dab7aba7c8189661e5af7a0c99d5
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-07-01 08:21:00 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,32 +1,95 @@
-const mainDiv = document.createElement('div');
-const ghostDiv = document.createElement('div');
+let arr = [];
 
-mainDiv.classList.add('maindiv_properties');
-ghostDiv.classList.add('ghostdiv_properties');
+for(let i = 1; i <= 100; i++) {
+    arr.push(i);
+}
 
-mainDiv.innerText = 'Hover your mouse over me mortal';
-ghostDiv.innerText = 'I\'m a ghost! Now you see me. Now you don\'t';
+arr.sort(() => 0.5 - Math.random());
 
-mainDiv.append(ghostDiv);
-document.body.append(mainDiv);
+// arr.sort(function(){ return 0.5 - Math.random()});
 
-// ********************************************************************************************************
+let table = document.createElement('table');
+table.setAttribute('id', 'table');
+// table.id = 'table';
 
-// const mainDiv = document.createElement('div');
-// const ghostDiv = document.createElement('div');
+let count = 0;
 
-// mainDiv.classList.add('maindiv_properties');
-// ghostDiv.classList.add('ghostdiv_properties');
+for (let i = 0; i < 10; i++) {
+  let tr = document.createElement('tr');
+  tr.classList.add('tr_td_properties');
 
-// mainDiv.innerText = 'Hover your mouse over me mortal';
-// ghostDiv.innerText = 'I\'m a ghost! Now you see me. Now you don\'t';
+  for (let j = 0; j < 10; j++) {
+    let td = document.createElement('td');
+    td.classList.add('tr_td_properties');
+    td.innerText = arr[count];
+    count++;
+    tr.append(td);
+  }
 
-// mainDiv.addEventListener('mouseenter', () => mainDiv.append(ghostDiv));
-// mainDiv.addEventListener('mouseout',() => ghostDiv.remove());
+  table.append(tr);
+}
 
-// // mainDiv.onmouseenter = createGhostDiv;
-// // mainDiv.onmouseout = removeGhostDiv;
+document.body.append(table);
 
-// document.body.append(mainDiv);
+// ***********************************************************************************************
 
+// let table = document.createElement('table');
+// table.setAttribute('id', 'table');
 
+// let count = 0;
+
+// for (let i = 0, arr = []; i < 10; i++) {
+//   arr = [];
+
+//   for (let j = 1; j <= 10; j++) {
+//     arr.push(i * 10 + j);
+//   }
+
+//   arr.sort(() => 0.5 - Math.random());
+
+//   let tr = document.createElement('tr');
+//   tr.classList.add('tr_td_properties');
+
+//   for (let j = 0; j < 10; j++) {
+//     let td = document.createElement('td');
+//     td.classList.add('tr_td_properties');
+//     td.innerText = arr[j];
+//     tr.append(td);
+//   }
+
+//   table.append(tr);
+// }
+
+// document.body.append(table);
+
+// ***********************************************************************************************
+
+// let arr = [];
+
+// for(let i = 1; i <= 100; i++) {
+//     arr.push(i);
+// }
+
+// arr.sort(function(){ return 0.5 - Math.random()});
+
+// let table = document.createElement('table');
+// table.setAttribute('id', 'table');
+
+// let count = 0;
+
+// for (let i = 0; i < 10; i++) {
+//   let tr = document.createElement('tr');
+//   tr.classList.add('tr_td_properties');
+
+//   for (let j = 0; j < 10; j++) {
+//     let td = document.createElement('td');
+//     td.classList.add('tr_td_properties');
+//     td.innerText = arr[count];
+//     count++;
+//     tr.append(td);
+//   }
+
+//   table.append(tr);
+// }
+
+// document.body.append(table);
