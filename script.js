0138c9325d3db5787c84b2f2a1cd044018361d54
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-07-02 15:06:25 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const mainDiv = document.createElement('div');
-const ghostDiv = document.createElement('div');
-
-mainDiv.classList.add('maindiv_properties');
-ghostDiv.classList.add('ghostdiv_properties');
-
-mainDiv.innerText = 'Hover your mouse over me mortal';
-ghostDiv.innerText = 'I\'m a ghost! Now you see me. Now you don\'t';
-
-mainDiv.append(ghostDiv);
-document.body.append(mainDiv);
-
-// ********************************************************************************************************
-
 // const mainDiv = document.createElement('div');
 // const ghostDiv = document.createElement('div');
 
@@ -21,12 +7,26 @@ document.body.append(mainDiv);
 // mainDiv.innerText = 'Hover your mouse over me mortal';
 // ghostDiv.innerText = 'I\'m a ghost! Now you see me. Now you don\'t';
 
-// mainDiv.addEventListener('mouseenter', () => mainDiv.append(ghostDiv));
-// mainDiv.addEventListener('mouseout',() => ghostDiv.remove());
-
-// // mainDiv.onmouseenter = createGhostDiv;
-// // mainDiv.onmouseout = removeGhostDiv;
-
+// mainDiv.append(ghostDiv);
 // document.body.append(mainDiv);
+
+// ********************************************************************************************************
+
+const mainDiv = document.createElement('div');
+const ghostDiv = document.createElement('div');
+
+mainDiv.classList.add('maindiv_properties');
+ghostDiv.classList.add('ghostdiv_properties');
+
+mainDiv.innerText = 'Hover your mouse over me mortal';
+ghostDiv.innerText = 'I\'m a ghost! Now you see me. Now you don\'t';
+
+mainDiv.addEventListener('mouseenter', () => mainDiv.append(ghostDiv));
+mainDiv.addEventListener('mouseout',() => ghostDiv.remove());
+
+// mainDiv.onmouseenter = createGhostDiv;
+// mainDiv.onmouseout = removeGhostDiv;
+
+document.body.append(mainDiv);
 
 
