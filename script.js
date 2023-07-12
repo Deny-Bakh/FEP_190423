dbcb7eb2f88d8d7b94cd490ee23fc9a59e103cd1
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-07-12 16:30:50 +0300

    work in progress

--- script.js
+++ script.js
@@ -24,3 +24,53 @@ const generateList = (array, deep) => {
 
 const array = [1, 2, [1, 2, [1, 2]], 3];
 document.body.append(generateList(array));
+
+// ***********************************************************************
+
+// const generateList = (array, deep) => {
+//   const ul = document.createElement('ul');
+
+//   array.forEach((item) => {
+//     const li = document.createElement('li');
+    
+//     if (Array.isArray(item)) {
+//       const newDeep = deep ? `${deep}.${item[0]}` : item[0];
+//       ul.append(generateList(item, newDeep));
+//     } else {
+//       const currentItem = deep ? `${deep}.${item}` : item;
+//       li.innerHTML = currentItem;
+//       ul.append(li);
+//     }
+//   });
+
+//   return ul;
+// };
+
+// const array = [1, 2, [1, 2, [1, 2]], 3];
+// document.body.append(generateList(array));
+
+// ***********************************************************************
+
+// const generateList = (array, deep) => {
+//   const ul = document.createElement('ul');
+
+//   array.forEach((item) => {
+//     const li = document.createElement('li');
+
+//     if (Array.isArray(item)) {
+//       const newDeep = deep ? `${deep}.${item[0]}` : item[0];
+//       const subUl = generateList(item, newDeep);
+//       li.appendChild(subUl);
+//     } else {
+//       const currentItem = deep ? `${deep}.${item}` : item;
+//       li.innerHTML = currentItem;
+//     }
+
+//     ul.appendChild(li);
+//   });
+
+//   return ul;
+// };
+
+// const array = [1, 2, [1, 2, [1, 2]], 3];
+// document.body.appendChild(generateList(array));
