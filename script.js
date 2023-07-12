b0310ea66c38bd0fd7a19d97caa52451b7ccd751
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-07-12 14:40:53 +0300

    work in progress

--- script.js
+++ script.js
@@ -6,89 +6,21 @@ const generateList = (array, deep) => {
   array.forEach((item) => {
     const li = document.createElement('li');
     if (Array.isArray(item)) {
-      li.append(generateList(item, 'deep'));
+      ul.append(generateList(item, previousLevel));
     } else {
       if (deep) {
-        li.innerHTML = `${previousLevel}.${item}`;
+        li.innerHTML = `${deep}.${item}`;
+        previousLevel = `${deep}.${item}`;
       } else {
         li.innerHTML = item;
         previousLevel = item;
       }
+      ul.append(li);
     }
-    ul.append(li);
   });
 
   return ul;
 };
 
-
-
 const array = [1, 2, [1, 2, [1, 2]], 3];
 document.body.append(generateList(array));
-
-
-const createElements = (tag, className, text) => {
-  const element = document.createElement(tag);
-  if (Array.isArray(className)) {
-    element.classList.add(...className);
-  } else {
-    element.classList.add(className);
-  }
-  if (text) {
-    element.innerText = text;  
-  }
-
-  return element;
-}
-
-const form = createElements('form', 'form');
-
-const nameLabel = createElements('label', 'label', 'Name');
-
-
-const userName = createElements('input', 'user_name');
-userName.required = true;
-
-const lastNameLabel = createElements('label', 'label', 'Last Name');
-
-const userLastName = createElements('input', 'user_last_name');
-userLastName.required = true;
-
-const birthLabel = createElements('label', 'label', 'Date of Birth');
-
-const dateOfBirth = createElements('input', 'date_of_birth');
-dateOfBirth.required = true;
-
-const sexLabel = document.createElement('label');
-sexLabel.innerText = 'Sex\n';
-
-const maleLabel = document.createElement('label');
-maleLabel.innerText = 'Male';
-const male = document.createElement('input');
-male.type = 'radio';
-male.name = 'sex';
-male.value = 'male';
-male.classList.add('male');
-
-console.log(male);
-
-const femaleLabel = document.createElement('label');
-femaleLabel.innerText = 'Female';
-const female = document.createElement('input');
-female.type = 'radio';
-female.name = 'sex';
-female.value = 'female';
-female.classList.add('female');
-
-const cityLabel = document.createElement('label');
-cityLabel.innerText = 'City:\n';
-const citySelect = document.createElement("select");
-citySelect.classList.add('city_select');
-citySelect.required = true;
-const cityOptions = ['Kyiv', 'Kharkiv', 'Lviv', 'Lutsk'];
-for (var i = 0; i < cityOptions.length; i++) {
-  var option = document.createElement("option");
-  option.value = cityOptions[i].toLowerCase();
-  option.textContent = cityOptions[i];
-  citySelect.appendChild(option);
-}
